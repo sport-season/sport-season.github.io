0875603d829afbd183fd69206219dd8598f6b2b6
Nikolay Fedoseev EnvyBook <xneek@mail.ru>
2021-10-27 21:59:49 +0300

    round

--- src/components/Activities/components/Widget/index.js
+++ src/components/Activities/components/Widget/index.js
@@ -9,7 +9,7 @@ const Widget = ({ distance, total_elevation_gain, average_speed, moving_time, co
     return <section className={styles.widgetContainer}>
         <Card title={'Дистанция'} unit={dU}>{d}</Card>
         <Card title={'Набор'} unit={hU}>{h}</Card>
-        <Card title={'Ср.скор.'} unit={'км/ч'}>{Math.round((average_speed/count) * 3.6 )}</Card>
+        <Card title={'Ср.скор.'} unit={'км/ч'}>{((average_speed/count) * 3.6 ).toFixed(2)}</Card>
         <Card title={'В движении'} unit="ч">{(moving_time / 60 / 60).toFixed(2)}</Card>
     </section>
 }
